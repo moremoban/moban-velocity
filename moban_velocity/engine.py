90db317f0909b39ec5d9cb9be55e5238560a6fc2
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-03 10:08:17 +0530

    :fire: Upgrade to moban 0.6

--- moban_velocity/engine.py
+++ moban_velocity/engine.py
@@ -1,16 +1,18 @@
 import codecs
 
 from airspeed import Template
+
+from moban.externals import file_system
 import moban.utils as utils
 
 
 class EngineVelocity(object):
-    def __init__(self, template_dirs, extensions=None):
-        self.template_dirs = template_dirs
+    def __init__(self, template_fs, extensions=None):
+        self.template_fs = template_fs
 
     def get_template(self, template_file):
         actual_file = utils.get_template_path(
-            self.template_dirs, template_file
+            self.template_fs, template_file
         )
         with codecs.open(actual_file, "r", encoding="utf-8") as source:
             template = Template(source.read())
