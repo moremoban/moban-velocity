188e1843c5ead9d0b4c0dcbf0c73c1ff58c37ede
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-03 10:20:17 +0530

    :fire: Support moban 0.6
    Closes #2

--- moban_velocity/engine.py
+++ moban_velocity/engine.py
@@ -3,17 +3,22 @@ import codecs
 from airspeed import Template
 
 from moban.externals import file_system
-import moban.utils as utils
 
 
 class EngineVelocity(object):
-    def __init__(self, template_fs, extensions=None):
+    def __init__(self, template_fs, options=None):
+        """
+        template_fs is a multfs instance and gives you the power to load
+        a template from equiped template directories.
+        :param fs.multifs.MultiFS template_fs: a MultiFS instance or a FS instance
+        :param options: a dictionary of potential parameters.
+                        not used yet.
+        """
         self.template_fs = template_fs
 
     def get_template(self, template_file):
-        actual_file = utils.get_template_path(
-            self.template_fs, template_file
-        )
+        template_file = file_system.to_unicode(template_file)
+        actual_file = self.template_fs.readtext(template_file)
         with codecs.open(actual_file, "r", encoding="utf-8") as source:
             template = Template(source.read())
         return template
